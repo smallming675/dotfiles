3a17e4494c3161349ffcc822fbec966fc483ee6a
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2026-01-31 00:09:12 +0800

    236 2026-01-31 00:09:02

--- configuration.nix
+++ configuration.nix
@@ -139,5 +139,16 @@
     flags = ["--print-build-logs"];
   };
 
+  services.mullvad-vpn.enable = true;
+  networking.nameservers = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = ["~."];
+    fallbackDns = ["1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one"];
+    dnsovertls = "true";
+  };
+
   system.stateVersion = "25.11";
 }

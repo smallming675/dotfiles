9b24a99c9b4b8b18ff2a8362e3b4d746621aa61a
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2026-01-31 18:58:42 +0800

    250 2026-01-31 18:58:36

--- home.nix
+++ home.nix
@@ -770,7 +770,7 @@ in {
       bind=SUPER,P,togglefloating
       bind=SUPER,Q,killclient
       bind=SUPER,U,spawn_shell,slurp | grim -g - - | wl-copy
-      bindr=,SUPER_L,spawn_shell,rofi -show drun || pkill rofi
+      bindr=NONE,SUPER_L,spawn_shell,pkill rofi || rofi -show drun
       bind=SUPER,O,spawn,obsidian
       bind=SUPER,6,view,1
       bind=SUPER,7,view,2

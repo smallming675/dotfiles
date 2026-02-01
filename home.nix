688d17e4119362426b312b664724b6b20ab1d9af
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2026-02-01 21:18:36 +0800

    299 2026-02-01 21:13:47

--- home.nix
+++ home.nix
@@ -259,6 +259,16 @@ in {
             vim.lsp.enable(servers)
           end,
         },
+        {
+            "kylechui/nvim-surround",
+            version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+            event = "VeryLazy",
+            config = function()
+                require("nvim-surround").setup({
+                    -- Configuration here, or leave empty to use defaults
+                })
+            end
+        }
       };
     '';
     extraConfig = ''

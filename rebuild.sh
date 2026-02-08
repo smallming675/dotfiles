99f89235abc64c8d17a0e982a3eafedb43a2629a
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2026-02-08 17:03:57 +0800

    nixos(desktop): switch to generation

--- rebuild.sh
+++ rebuild.sh
@@ -27,3 +27,25 @@ else
 fi
 
 echo "Rebuild success for ${host}."
+
+
+#!/usr/bin/env bash
+set -e
+
+host="${1:-desktop}"
+REPO_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+cd "$REPO_DIR"
+
+alejandra . &>/dev/null \
+  || ( alejandra . ; echo "formatting failed!" && exit 1)
+
+git diff -U0 '*.nix'
+
+echo "NixOS Rebuilding..."
+
+sudo nixos-rebuild switch --flake "${REPO_DIR}#${host}"
+current=$(nixos-rebuild list-generations | awk '$NF=="True" {print $1, $2, $3}')
+
+git commit -am "$current"
+
+echo "Rebuild success!"

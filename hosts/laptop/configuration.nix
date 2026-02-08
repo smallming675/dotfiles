5fa142947b36ae27227c73c49250877d5746d490
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2026-02-08 17:56:41 +0800

    manual config update

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -6,19 +6,10 @@
   imports = [
     ../../modules/nixos/common.nix
     ./hardware-configuration.nix
-    inputs.nixos-hardware.nixosModules.microsoft-surface-common
   ];
 
   config = {
     networking.hostName = "laptop";
     networking.networkmanager.wifi.backend = "iwd";
-    # hardware.microsoft-surface.kernelVersion = "stable";
-    #
-    # environment.systemPackages = with pkgs; [
-    #   surface-control
-    # ];
-    #
-    # users.users.user.extraGroups = ["surface-control"];
-    # services.xserver.videoDrivers = ["modesetting"];
   };
 }

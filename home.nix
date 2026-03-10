abddc1be68ae2ba60a9562a5cd246459eef6710b
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2026-03-10 14:12:59 +0800

    477 2026-03-10 14:12:56

--- home.nix
+++ home.nix
@@ -1032,7 +1032,7 @@ in {
     };
     Service = {
       Type = "oneshot";
-      ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd --trust --path / ${config.home.homeDirectory}/sync https://user:NTYJ8-E895T-iDjcZ-ggenj-Px9T2@${nextCloudDomain}/";
+      ExecStart = "${pkgs.nextcloud-client}/bin/nextcloudcmd --trust --path / ${config.home.homeDirectory}/sync https://root:ZA2bP-gYPsy-Azaj8-yZy9H-PM6Bg@${nextCloudDomain}/remote.php/dav/files/root/";
     };
   };
   

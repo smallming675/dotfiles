c7a583b74a28067c9dc580f6776eb0b8f6c1dd55
SmallMing675 <181466198+smallming675@users.noreply.github.com>
2026-03-18 18:02:01 +0800

    498 2026-03-18 18:01:58

--- home.nix
+++ home.nix
@@ -994,7 +994,7 @@ in {
      settings = {
        theme = "tokyonight";
        model = "anthropic/claude-sonnet-4-6";
-       plugin = [ "cc-safety-net" "@tarquinen/opencode-dcp@latest" "micode" "opencode-snip@latest" "@bastiangx/opencode-unmoji" "superpowers@git+https://github.com/obra/superpowers.git" ];
+       plugin = [ "cc-safety-net" "@tarquinen/opencode-dcp@latest" "micode" "@bastiangx/opencode-unmoji" "superpowers@git+https://github.com/obra/superpowers.git" ];
        provider = {
          anthropic = {
            options = {
